cad7dfe85208488df47120ad2dd42298a14663d7
Berrin Altinsoy <berrinaltinsoy3@gmail.com>
2023-07-28 13:48:19 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Berrin on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
